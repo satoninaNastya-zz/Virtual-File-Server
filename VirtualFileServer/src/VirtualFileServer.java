417d16ca2ba872a4dabeeba02beebd848a0b6389
Satonina Anastasia <satonina@mercurydevelopment.com>
2014-05-27 16:35:25 +0400

    сервер

---
+++ VirtualFileServer/src/VirtualFileServer.java
@@ -0,0 +1,69 @@
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Properties;
+
+public class VirtualFileServer {
+
+    private static ArrayList<ClientThread> listClient = new ArrayList<ClientThread>();
+    private static VirtualFileSystem virtualFileSystem = new VirtualFileSystem("C:", listClient);
+
+    public static void main(String[] args) throws IOException {
+
+        ServerSocket serverSocket;
+        serverSocket = null;
+        try {
+            int port = gerPortFromFile("config.properties");
+            if (port == -1) {
+                System.out.println("Error config file");
+                return;
+            }
+
+            try {
+                serverSocket = new ServerSocket(port);
+            } catch (IOException e) {
+                System.out.println("Error server did not start");
+            }
+            System.out.println("Server run");
+
+            while (true) try {
+                assert serverSocket != null;
+                Socket socket = serverSocket.accept();
+                ClientThread client = new ClientThread(socket, virtualFileSystem);
+                listClient.add(client);
+
+            } catch (NullPointerException e) {
+                System.out.println("Error");
+                break;
+
+            }
+        } finally {
+            if (serverSocket != null)
+                serverSocket.close();
+
+        }
+    }
+
+    private static int gerPortFromFile(String configFile) {
+        Properties properties = new Properties();
+        InputStream inputStream = null;
+        try {
+            inputStream = new FileInputStream(configFile);
+            properties.load(inputStream);
+            String port = properties.getProperty("port");
+            return Integer.parseInt(port);
+        } catch (IOException io) {
+            System.out.print("configuration file is not found");
+            return -1;
+        } finally {
+            if (inputStream != null) {
+                try {
+                    inputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
