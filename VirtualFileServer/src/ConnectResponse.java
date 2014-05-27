752cada218190e32920e7c95366f4319f247adb7
Satonina Anastasia <satonina@mercurydevelopment.com>
2014-05-27 16:41:48 +0400

    ответ пользователю после выполнения  команды connect

---
+++ VirtualFileServer/src/ConnectResponse.java
@@ -0,0 +1,19 @@
+
+public class ConnectResponse implements Response {
+    private ClientThread theClientThread;
+    private int numberClientOnline;
+
+    public ConnectResponse(ClientThread clientThread, int numberClient) {
+        this.numberClientOnline=numberClient;
+        this.theClientThread=clientThread;
+    }
+
+    @Override
+    public void send() {
+        String response;
+        if(numberClientOnline==1)
+        response ="Online "+numberClientOnline+" user now";
+        else response ="Online "+numberClientOnline+" users now";
+        theClientThread.sendMessage(response);
+    }
+}
