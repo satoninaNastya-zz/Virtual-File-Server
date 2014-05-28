a92abafca9afa0552a6c370bafae326a3c6f5098
Satonina Anastasia <satonina@mercurydevelopment.com>
2014-05-28 16:58:31 +0400

    исправлены исключения

--- VirtualFileServer/src/Client.java
+++ VirtualFileServer/src/Client.java
@@ -1,81 +1,112 @@
 import java.io.*;
+import java.net.ConnectException;
 import java.net.Socket;
+import java.net.SocketException;
+import java.net.UnknownHostException;
 
 public class Client {
     private static BufferedReader keyboardReader = new BufferedReader(new InputStreamReader(System.in));
     private static ObjectOutputStream outputServer;
     private static CommandGetter getCommandFromUser;
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         String commandString;
         Command newCommand;
-
-        connectClientWithServer();
+        boolean isConnect = false;
+        while (!isConnect) {
+            isConnect = connectClientWithServer();
+        }
         try {
             while (true) {
-
                 commandString = keyboardReader.readLine();
                 if (commandString != null) {
                     if ("quit".compareTo(commandString) == 0) {
                         QuiteUserCommand quit = new QuiteUserCommand();
                         outputServer.writeObject(quit);
                         outputServer.flush();
+                        System.exit(0);
                         break;
                     }
-                        newCommand = getCommandFromUser.getCommand(commandString);
-                        if (newCommand != null) {
-                            outputServer.writeObject(newCommand);
-                            outputServer.flush();
-                        } else System.out.println("Incorrect command");
-
+                    newCommand = getCommandFromUser.getCommand(commandString);
+                    if (newCommand != null) {
+                        outputServer.writeObject(newCommand);
+                        outputServer.flush();
+                    } else System.out.println("Incorrect command");
                 }
             }
-        } catch (Exception e) {
-            e.printStackTrace();
-            System.out.println("Error command");
+        } catch (SocketException se) {
+            outputServer = null;
+            System.err.println("Server is not running");
+            System.exit(0);
+        } catch (IOException io) {
+            io.printStackTrace();
         } finally {
-            QuiteUserCommand quit = new QuiteUserCommand();
-            outputServer.writeObject(quit);
-            outputServer.flush();
-            System.out.println("closing...");
+            try {
+                keyboardReader.close();
+                if (outputServer != null)
+                    outputServer.close();
+            } catch (IOException io) {
+                io.printStackTrace();
+            }
         }
     }
 
-    private static void connectClientWithServer() {
+    private static boolean connectClientWithServer() {
         Socket socket;
         String connectString;
         BufferedReader inputServer;
         String response;
-        try {
-            while (true) {
+
+        while (true) {
+            try {
                 connectString = keyboardReader.readLine();
-                String words[] = connectString.split(" ");
-                if (words.length == 3) {
-                    if (words[0].compareToIgnoreCase("connect") == 0) {
-                        ConnectCommand connectCommand = new ConnectCommand(words[1], words[2]);
-                        if (connectCommand.isCorrect()) {
-                            socket = new Socket(connectCommand.getAddress(), connectCommand.getPort());   // 127.0.0.1:1255
-                            getCommandFromUser = new CommandGetter();
-
-                            outputServer = new ObjectOutputStream(socket.getOutputStream());
-                            outputServer.writeObject(connectCommand);
-                            outputServer.flush();
-
-                            inputServer = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-
-                            response = inputServer.readLine();
-                            System.out.println(response);
-                            String wordsResponse[] = response.split(" ");
-                            if ("Online".equals(wordsResponse[0])) {
-                                ReadMessageFromServerThread readFromServerThread = new ReadMessageFromServerThread(socket);
-                                return;
-                            }
-                        }
-                    }
-                }
+            } catch (IOException io) {
+                io.printStackTrace();
+                return false;
             }
-        } catch (Exception e) {
-            e.printStackTrace();
+            if (connectString == null)
+                return false;
+            String words[] = connectString.split(" ");
+            if (words.length != 3)
+                return false;
+            if (words[0].compareToIgnoreCase("connect") != 0)
+                return false;
+            ConnectCommand connectCommand = new ConnectCommand(words[1], words[2]);
+
+            try {
+                socket = new Socket(connectCommand.getAddress(), connectCommand.getPort());   // 127.0.0.1:1255 или localhost:1255
+            } catch (UnknownHostException uh) {
+                System.err.println("incorrect address");
+                return false;
+            } catch (ConnectException c) {
+                System.err.println("incorrect port or server is not running");
+                return false;
+            } catch (IOException io) {
+                System.out.print("You not connect");
+                io.printStackTrace();
+                return false;
+            }
+            getCommandFromUser = new CommandGetter();
+
+            try {
+                outputServer = new ObjectOutputStream(socket.getOutputStream());
+                outputServer.writeObject(connectCommand);
+                outputServer.flush();
+                inputServer = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                response = inputServer.readLine();
+                System.out.println(response);
+                String wordsResponse[] = response.split(" ");
+                if ("Online".equals(wordsResponse[0])) {
+                    ReadMessageFromServerThread readFromServerThread = new ReadMessageFromServerThread(socket);
+                    return true;
+                }
+            } catch (IOException io) {
+                System.out.print("You not connect");
+                io.printStackTrace();
+                return false;
+            }
+            return false;
         }
     }
+
 }
